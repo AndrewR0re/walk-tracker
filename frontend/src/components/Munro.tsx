d4d551da5dcb6eb61e7ced9694928b2051e04bc4
Andrew Rore <Andrewrore94@gmail.com>
2026-08-31 13:57:54 +0100

    Added altitude in meters to munro

--- frontend/src/components/Munro.tsx
+++ frontend/src/components/Munro.tsx
@@ -5,13 +5,18 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
 export default function Munro({ name, altitude, date }: MunroProps) {
+    const convertFeetToMeters = (feet: number) => {
+        const meters = feet / 3.28084;
+        return Number(meters.toFixed(2));
+    }
+    
     return (
         <Box sx={{ width: '100%', pt: 2 }}>
             <Card sx={{ width: '100%' }}>
                 <CardContent>
                     <Typography variant='h5'>{name}</Typography>
                     <Typography variant='body1' color='text.secondary'>
-                        Altitude: {altitude}m
+                        Altitude: {altitude} feet / {convertFeetToMeters(altitude)} meters
                     </Typography>
                     <Typography variant='body1' color='text.secondary'>
                         Climbed on: {date}
